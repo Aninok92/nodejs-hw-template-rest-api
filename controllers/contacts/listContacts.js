5deb889913c36111817ba64e21f13814c85cda2b
Nina Vasilieva <aninok92@gmail.com>
2021-10-11 16:18:03 +0300

    added pagination

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,10 +2,13 @@ const { Contact } = require('../../models')
 const { sendSuccessResponse } = require('../../helpers')
 
 const listContacts = async (req, res) => {
+  const { page = 1, limit = 20 } = req.query
+  const skip = (page - 1) * limit
   const { _id } = req.user
   const result = await Contact.find(
     { owner: _id },
-    '_id name email phone favorite'
+    '_id name email phone favorite',
+    { skip, limit: +limit }
   )
   sendSuccessResponse(res, { data: result })
 }
